d9ad98bc06a48fa0840bfd64ee6ebc6c09fd96fa
CodinGitHub <gms2programador@gmail.com>
2022-03-15 22:52:35 -0500

    game array done

---
+++ main.js
@@ -0,0 +1,57 @@
+let gameArray = [];
+let playerArray = [];
+
+mainLoop();
+
+function mainLoop() {
+
+    // reseteo del ingreso del usuario
+    playerArray = [];
+
+    // 1. Generar numero aleatorio
+    let randomNumber = Math.floor(Math.random()*4 + 1)
+
+    // 2. Agregar numero aleatorio al arreglo del juego
+    gameArray.push(randomNumber);
+
+    // 3. Mostrar secuencia
+    console.log("Simon Says: " + gameArray);
+
+    // 4. Esperar ingreso
+    
+}
+
+let userInput = (input) => {
+
+    if (playerArray.length < gameArray.length){
+        //5. Agregar ingreso del usuario al arreglo del usuario
+        playerArray.push(input);
+        if (playerArray.length < gameArray.length){
+            if(compare(playerArray, gameArray) == true){
+                mainLoop()
+            }else{
+                console.log("You Lost");
+            }
+        }
+    }else{
+        if(compare(playerArray, gameArray) == true){
+            mainLoop()
+        }else{
+            console.log("You Lost");
+        }
+    }
+}
+
+
+let compare = (array1, array2) =>{
+    // console.log(gameArray);
+    console.log(playerArray);
+    for(let i=0; i<array1.length ; i++){
+        if (array1[i] == array2[i]){
+            //
+        }else{
+            return false
+        }
+    }
+    return true;
+}
